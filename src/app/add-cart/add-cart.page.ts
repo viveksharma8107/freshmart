18c77f01cda662df8032f017a61087f5fa599369
vivek sharma <viveksharma8107@gmail.com>
2020-04-19 03:21:31 +0530

    Homepage

---
+++ src/app/add-cart/add-cart.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-cart',
+  templateUrl: './add-cart.page.html',
+  styleUrls: ['./add-cart.page.scss'],
+})
+export class AddCartPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'add-cart',
+    loadChildren: () => import('./add-cart/add-cart.module').then( m => m.AddCartPageModule)
+  },
 ];
 
 @NgModule({
